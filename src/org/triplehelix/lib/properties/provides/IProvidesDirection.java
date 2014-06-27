fc06f55f8ff0cf43f41f485bee1d346655e0557c
lythgoe.matthew@gmail.com <lythgoe.matthew@gmail.com@af1843c3-bd72-e556-1431-297ac1fb766f>
2014-06-27 17:29:14 +0000

    Updating java doc for all IProvidesDirection.

---
+++ src/org/triplehelix/lib/properties/provides/IProvidesDirection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.triplehelix.lib.properties.provides;
+
+/**
+ * Object has a direction.
+ * @author robotics
+ */
+public interface IProvidesDirection {
+    
+    /**
+     * Retrieves the direction of the object.
+     * @return The direction of the object.
+     */
+    public String getDirection();  
+}
